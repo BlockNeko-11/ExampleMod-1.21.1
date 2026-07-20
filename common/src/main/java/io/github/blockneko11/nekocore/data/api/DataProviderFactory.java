34cdb7c1a25b3005bec964d4ec32e957a098ff49
BlockNeko-11 <block_mrlimr267@outlook.com>
2026-07-20 17:54:02 +0800

    remove unecessary files and add data gen

--- common/src/main/java/io/github/blockneko11/nekocore/data/api/DataProviderFactory.java
+++
@@ -1,9 +0,0 @@
-package io.github.blockneko11.nekocore.data.api;
-
-import net.minecraft.data.DataOutput;
-import net.minecraft.data.DataProvider;
-
-@FunctionalInterface
-public interface DataProviderFactory<T extends DataProvider> {
-    T create(String modId, boolean validate, DataOutput output);
-}
--- common/src/main/java/io/github/blockneko11/nekocore/event/bus/api/Event.java
+++
@@ -1,4 +0,0 @@
-package io.github.blockneko11.nekocore.event.bus.api;
-
-public abstract class Event {
-}
